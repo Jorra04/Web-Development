2f2d3e7b62fa7b9396a674b0c4fedb68b4f843ae
jorra04 <jorra04@my.yorku.ca>
2021-04-10 16:20:51 -0400

    Fixed the logic for using while loop and calling async functions.

--- Practice/BlackJack.js
+++ Practice/BlackJack.js
@@ -3,15 +3,15 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 //Function to check if ace should be treated as high or low
 const aceHighOrLow = (players_current_value) => {
-    if(Number(players_current_value) + 11 > 21) {
+    if (Number(players_current_value) + 11 > 21) {
         return 1;
     }
     return 11;
 }
 
 //function to check if the the card is a face card.
-const isFaceCard = (card) =>{
-  
+const isFaceCard = (card) => {
+
     return (
         card.value === 'JACK' ||
         card.value === 'QUEEN' ||
@@ -23,11 +23,11 @@ const isFaceCard = (card) =>{
 const getValues = (cards) => {
     let players_current_value = 0;
     cards.forEach(card => {
-        
-        if(isFaceCard(card)){
+
+        if (isFaceCard(card)) {
             players_current_value += 10;
         }
-        else if(card.value === 'ACE'){
+        else if (card.value === 'ACE') {
             players_current_value += aceHighOrLow(players_current_value);
         }
         else {
@@ -44,7 +44,7 @@ const getValues = (cards) => {
 const start = async () => {
     let dealers_card = [];
     let players_cards = [];
-    
+
     try {
         const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6');
         const deckID = resp.data.deck_id;
@@ -56,11 +56,11 @@ const start = async () => {
         let dealer_blackjack = getValues(dealers_card) === 21;
 
         return ({
-            deck_id : deckID,
-            players_cards : players_cards,
-            player_blackjack : player_blackjack,
-            dealers_card : dealers_card,
-            dealer_blackjack : dealer_blackjack
+            deck_id: deckID,
+            players_cards: players_cards,
+            player_blackjack: player_blackjack,
+            dealers_card: dealers_card,
+            dealer_blackjack: dealer_blackjack
 
         });
 
@@ -69,107 +69,54 @@ const start = async () => {
     }
 }
 
-const getInput = async () => {
-    let guess = prompt("Enter a value: ");
 
-    return guess;
+const dealCards = async (deck_identifier) => {
+    const dealtCardsResp = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_identifier}/draw/?count=1`);
+    return getValues(dealtCardsResp.data.cards);
+}
+
+
+
+const getInput = (text) => {
+    let guess = prompt(text);
+
+    if (guess === 'y' || guess === 'Y') {
+
+        return true;
+    }
+    return false;
 
 }
 
 
 start()
-.then((deck_information) => {
-    console.log(deck_information);
-    // console.log("====== value ======");
-    // console.log(getValues(deck_information.first_dealt));
-})
+    .then(async (deck_information) => {
 
+        if (deck_information.player_blackjack) {
+            console.log("Congratulations! You have BlackJack.");
+        }
+        else if (deck_information.dealer_blackjack) {
+            console.log("The dealer has BlackJack.");
+        }
+        else {
+            let player_score = getValues(deck_information.players_cards);
+            let dealer_score = getValues(deck_information.dealers_card);
 
+            while (getInput(`You currently have ${player_score} and the dealer has ${dealer_score}. Would you like to hit?`)) {
+                
+                try {
+                    const addedVal = await dealCards(deck_information.deck_id);
+                    player_score+= addedVal;
+                    console.log(player_score);
+                } catch(err) {
+                    console.log(err.message);
+                }
+            }
 
+            console.log('dealer is hitting now');
+        }
 
-
-
-
-
-
-
-
-
-
-
-// let deckID = '';
-// let current_value = 0;
-// const start = async () => {
-//     try {
-//         const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6');
-//         return resp.data.deck_id;
-
-//     } catch (err) {
-//         console.log(err.message);
-//     }
-// }
-
-// const deal = async (deck_id, first_hand) => {
-//     let card = '';
-//     try {
-//         if (first_hand) {
-//             const resp = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`);
-//             card = resp.data.cards;
-//         }
-//         else {
-//             const resp = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
-
-//             card = resp.data.cards;
-//         }
-
-//         return card;
-
-
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-
-
-// const deck_identifier = start();
-
-// console.log(deck_identifier);
-
-
-/*
-This code works, however, it is an example of callback hell.
-Need to find a better way to write this with async and await.
-*/
-
-// start()
-//     .then(deck_id => {
-
-//         deal(deck_id, true)
-//             .then(cards => {
-//                 cards.forEach(card => {
-//                     console.log(card.value + " of " + card.suit);
-//                 });
-//             })
-//             .catch(err => {
-//                 console.log(err.message);
-//             })
-//             .then(() => {
-//                 let i = 0;
-
-//                 while (i < 5) {
-//                     deal(deck_id, false)
-//                         .then(cards => {
-//                             cards.forEach(card => {
-//                                 console.log(card.value + " of " + card.suit);
-//                             });
-//                         })
-//                         .catch(err => {
-//                             console.log(err.message);
-//                         })
-//                     i++;
-//                 }
-//             })
-//     })
-//     .catch(err => {
-//         console.log(err.message);
-//     })
+    })
+    .catch(err => {
+        console.log(err.message);
+    })

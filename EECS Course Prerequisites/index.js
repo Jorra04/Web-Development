f0137bfc3971d632a2dc527d3c1ed0c9dccf8e8b
unknown <jorra04@my.yorku.ca>
2021-04-17 18:07:53 -0400

    Split up some files and worked with adding some annimation to the project.

--- EECS Course Prerequisites/index.js
+++ EECS Course Prerequisites/index.js
@@ -1,10 +1,33 @@
 const findPrereq = () => {
-    let prerequisite = document.getElementById("course-number").value;
+  let prerequisite = document.getElementById("course-number").value;
+  let courses = [
+    "LE/EECS 1021",
+    "LE/EECS 1022",
+    "LE/EECS 1023",
+    "LE/EECS 1024",
+    "LE/EECS 1025",
+    "LE/EECS 1026",
+  ];
 
-    if(prerequisite.length === 4){
-        console.log('acceptable');
-    } else {
-        console.log('The course code must be 4 digits in length');
-    }
+  if (prerequisite.length === 4) {
 
-}
+    let ul = document.getElementById("dynamic-list"); //Get the unordered list we want to manipulate.
+    
+    ul.querySelectorAll('*').forEach(n => n.remove()); //Remove all the current list items.
+    
+
+    //Add courses
+    courses.forEach((course) => {
+      
+      let li = document.createElement("li");
+      li.setAttribute("id", prerequisite);
+      li.appendChild(document.createTextNode(course));
+      ul.appendChild(li);
+    });
+
+    //Reset textbox after operations have been preformed
+    document.getElementById("course-number").value = "";
+  } else {
+    console.log("The course code must be 4 digits in length");
+  }
+};

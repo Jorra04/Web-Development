1591c30fd4d9606bb8b645e380cc59db3273bdef
unknown <jorra04@my.yorku.ca>
2021-04-25 13:17:40 -0400

    Added the ability to search courses requiring a specific course as well.

--- EECS Course Prerequisites/index.js
+++ EECS Course Prerequisites/index.js
@@ -1,7 +1,7 @@
 const findPrereq = async () => {
   let course_code = document.getElementById("course-number").value;
   let course_faculty = document.getElementById("faculties").value;
-
+  let courses = [];
   let refined_course_faculty = course_faculty.replace("/", "%2f").trim();
   let response_received = false;
   if (course_code.length === 4) {
@@ -9,11 +9,11 @@ const findPrereq = async () => {
       const resp = await axios.get(
         `http://yorkapi-env.eba-fi5ekpb4.us-east-2.elasticbeanstalk.com/prerequisites/coursesRequired/${refined_course_faculty}/${course_code}`
       );
-      
-      let courses = resp.data.courses;
+
+      courses = resp.data.courses;
 
       response_received = typeof courses != typeof undefined;
-    
+
       document.getElementById(
         "your-courses-header"
       ).innerHTML = `Found Prerequistes for ${course_faculty} ${course_code}`;
@@ -27,16 +27,20 @@ const findPrereq = async () => {
       let ul = document.getElementById("dynamic-list"); //Get the unordered list we want to manipulate.
 
       ul.querySelectorAll("*").forEach((n) => n.remove()); //Remove all the current list items.
+      if (courses.length == 0) {
+        document.getElementById(
+          "your-courses-header"
+        ).innerHTML = `"${course_faculty} ${course_code}" does not have any prerequisites.`;
+      } else {
+        //Add courses
+        courses.forEach((course) => {
+          let li = document.createElement("li");
+          li.setAttribute("id", course_code);
+          li.appendChild(document.createTextNode(course));
+          ul.appendChild(li);
+        });
+      }
 
-      //Add courses
-      courses.forEach((course) => {
-        let li = document.createElement("li");
-        li.setAttribute("id", course_code);
-        li.appendChild(document.createTextNode(course));
-        ul.appendChild(li);
-      });
-
-      
     } else {
       document.getElementById(
         "your-courses-header"
@@ -45,7 +49,64 @@ const findPrereq = async () => {
 
     //Reset textbox after operations have been preformed
     document.getElementById("course-number").value = "";
+  } else {
+    alert(`The Course Code needs to be exactly 4 digits in length.`);
+  }
+};
 
+
+
+
+const findPostreq = async () => {
+  let course_code = document.getElementById("course-number").value;
+  let course_faculty = document.getElementById("faculties").value;
+  let courses = [];
+  let refined_course_faculty = course_faculty.replace("/", "%2f").trim();
+  let response_received = false;
+  if (course_code.length === 4) {
+    try {
+      const resp = await axios.get(
+        `http://yorkapi-env.eba-fi5ekpb4.us-east-2.elasticbeanstalk.com/prerequisites/coursesRequiring/${refined_course_faculty}/${course_code}`
+      );
+
+      courses = resp.data.courses;
+
+      response_received = typeof courses != typeof undefined;
+
+      document.getElementById(
+        "your-courses-header"
+      ).innerHTML = `Found Prerequistes for ${course_faculty} ${course_code}`;
+    } catch (err) {
+      document.getElementById(
+        "your-courses-header"
+      ).innerHTML = `"${course_faculty} ${course_code}" is either not recognized by this program or does not exist!`;
+    }
+
+    if (response_received) {
+      let ul = document.getElementById("dynamic-list"); //Get the unordered list we want to manipulate.
+
+      ul.querySelectorAll("*").forEach((n) => n.remove()); //Remove all the current list items.
+      if (courses.length == 0) {
+        document.getElementById(
+          "your-courses-header"
+        ).innerHTML = `No courses require the following course: ${course_faculty} ${course_code}.`;
+      } else {
+        //Add courses
+        courses.forEach((course) => {
+          let li = document.createElement("li");
+          li.setAttribute("id", course_code);
+          li.appendChild(document.createTextNode(course));
+          ul.appendChild(li);
+        });
+      }
+    } else {
+      document.getElementById(
+        "your-courses-header"
+      ).innerHTML = `"${course_faculty} ${course_code}" is either not recognized by this program or does not exist!`;
+    }
+
+    //Reset textbox after operations have been preformed
+    document.getElementById("course-number").value = "";
   } else {
     alert(`The Course Code needs to be exactly 4 digits in length.`);
   }

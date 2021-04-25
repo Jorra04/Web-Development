c057c2e194e445cf59482617618077107b8f9a10
unknown <jorra04@my.yorku.ca>
2021-04-25 12:54:54 -0400

    Added a header to the courses found. Also found a way to see if the course was found by the api or not.

--- EECS Course Prerequisites/index.js
+++ EECS Course Prerequisites/index.js
@@ -1,41 +1,52 @@
-const findPrereq = () => {
-  let prerequisite = document.getElementById("course-number").value;
-  let courses = [
-    "LE/EECS 1021",
-    "LE/EECS 1022",
-    "LE/EECS 1023",
-    "LE/EECS 1024",
-    "LE/EECS 1025",
-    "LE/EECS 1026",
-    "LE/EECS 1023",
-    "LE/EECS 1024",
-    "LE/EECS 1025",
-    "LE/EECS 1026",
-    "LE/EECS 1023",
-    "LE/EECS 1024",
-    "LE/EECS 1025",
-    "LE/EECS 1026",
-  ];
+const findPrereq = async () => {
+  let course_code = document.getElementById("course-number").value;
+  let course_faculty = document.getElementById("faculties").value;
 
-  if (prerequisite.length === 4) {
-
-    let ul = document.getElementById("dynamic-list"); //Get the unordered list we want to manipulate.
-    
-    ul.querySelectorAll('*').forEach(n => n.remove()); //Remove all the current list items.
-    
-
-    //Add courses
-    courses.forEach((course) => {
+  let refined_course_faculty = course_faculty.replace("/", "%2f").trim();
+  let response_received = false;
+  if (course_code.length === 4) {
+    try {
+      const resp = await axios.get(
+        `http://yorkapi-env.eba-fi5ekpb4.us-east-2.elasticbeanstalk.com/prerequisites/coursesRequired/${refined_course_faculty}/${course_code}`
+      );
       
-      let li = document.createElement("li");
-      li.setAttribute("id", prerequisite);
-      li.appendChild(document.createTextNode(course));
-      ul.appendChild(li);
-    });
+      let courses = resp.data.courses;
+
+      response_received = typeof courses != typeof undefined;
+    
+      document.getElementById(
+        "your-courses-header"
+      ).innerHTML = `Found Prerequistes for ${course_faculty} ${course_code}`;
+    } catch (err) {
+      document.getElementById(
+        "your-courses-header"
+      ).innerHTML = `"${course_faculty} ${course_code}" is either not recognized by this program or does not exist!`;
+    }
+
+    if (response_received) {
+      let ul = document.getElementById("dynamic-list"); //Get the unordered list we want to manipulate.
+
+      ul.querySelectorAll("*").forEach((n) => n.remove()); //Remove all the current list items.
+
+      //Add courses
+      courses.forEach((course) => {
+        let li = document.createElement("li");
+        li.setAttribute("id", course_code);
+        li.appendChild(document.createTextNode(course));
+        ul.appendChild(li);
+      });
+
+      
+    } else {
+      document.getElementById(
+        "your-courses-header"
+      ).innerHTML = `"${course_faculty} ${course_code}" is either not recognized by this program or does not exist!`;
+    }
 
     //Reset textbox after operations have been preformed
     document.getElementById("course-number").value = "";
+
   } else {
-    console.log("The course code must be 4 digits in length");
+    alert(`The Course Code needs to be exactly 4 digits in length.`);
   }
 };

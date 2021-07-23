17fb5605d08693187a77226787c058017230a25d
unknown <jorra04@my.yorku.ca>
2021-07-23 19:12:02 -0400

    Changed name

---
+++ nhl-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import Teams from './Teams'
+
+const App = () => {
+
+  return (
+    <>
+      <Teams/>
+    </>
+  );
+}
+
+export default App;

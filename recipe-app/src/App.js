a508d2e14c4e1903b05193a9b8c1bd3c9487ce28
unknown <jorra04@my.yorku.ca>
2021-07-23 18:27:42 -0400

    Got team searching to work/

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -6,8 +6,23 @@ import logo from './logo.png'
 const App = () => {
 
   const exampleReq = `https://statsapi.web.nhl.com/api/v1/teams/`;
-
   const [teams, setTeams] = useState([]);
+
+  const [teamsOriginal, setTeamsOriginal] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+
+  const updateSearch = e => {
+    setSearchText(e.target.value);
+    let newTeams = teamsOriginal.filter((team) => {
+      return team.name.toLowerCase().includes(e.target.value);
+    });
+
+    setTeams(newTeams);
+  }
+
+
   useEffect( async () => {
     getData();
   }, []);
@@ -16,6 +31,7 @@ const App = () => {
     const resp = await fetch(exampleReq);
     const data = await resp.json();
     setTeams(data.teams);
+    setTeamsOriginal(data.teams);
   }
 
 
@@ -23,7 +39,7 @@ const App = () => {
     <div className="App">
       <form className="search-form">
         <img className="logo" src={logo}></img>
-        <input className="search-bar" type="text"/>
+        <input className="search-bar" type="text" value={searchText} onChange={updateSearch}/>
         <button className="search-button"type="submit">Submit</button>
       </form>
       <div className="teams">

2a0b8757388f869ee3b3a765920a578278f3ecc9
unknown <jorra04@my.yorku.ca>
2021-07-23 19:10:24 -0400

    Separated the Teams stuff.

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,53 +1,13 @@
 import './App.css';
-import Team from './Team'
 import React, {useEffect, useState} from 'react';
-import logo from './logo.png'
+import Teams from './Teams'
 
 const App = () => {
 
-  const exampleReq = `https://statsapi.web.nhl.com/api/v1/teams/`;
-  const [teams, setTeams] = useState([]);
-
-  const [teamsOriginal, setTeamsOriginal] = useState([]);
-
-  const [searchText, setSearchText] = useState("");
-
-
-  const updateSearch = e => {
-    setSearchText(e.target.value);
-    let newTeams = teamsOriginal.filter((team) => {
-      return team.name.toLowerCase().includes(e.target.value);
-    });
-
-    setTeams(newTeams);
-  }
-
-
-  useEffect( async () => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    const resp = await fetch(exampleReq);
-    const data = await resp.json();
-    setTeams(data.teams);
-    setTeamsOriginal(data.teams);
-  }
-
-
   return (
-    <div className="App">
-      <form className="search-form">
-        <img className="logo" src={logo}></img>
-        <input className="search-bar" type="text" value={searchText} onChange={updateSearch}/>
-        <button className="search-button"type="submit">Submit</button>
-      </form>
-      <div className="teams">
-      {teams.map(team => (
-        <Team key={team.abbreviation} name={team.name} abbreviation={team.abbreviation} established={team.firstYearOfPlay}/>
-      ))}
-      </div>
-    </div>
+    <>
+      <Teams/>
+    </>
   );
 }
 
